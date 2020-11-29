3bb279841301d6e2191680cacccadd90304e3af7
Herman_Bilous <Herman_Bilous@epam.com>
2020-11-29 23:00:38 +0200

    Add cart api integration

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,8 @@ const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://julbe5p894.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://cc3mwqo2y1.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'https://cc3mwqo2y1.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://wed4qpuik0.execute-api.eu-west-1.amazonaws.com/dev/api',
 };
 
 export default API_PATHS;
